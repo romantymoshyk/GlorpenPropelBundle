a803304f07ef46b86afc6aa8036714e2c88a1e65
Roman Tymoshyk <roman_tymoshyk@epam.com>
2019-06-19 14:07:31 +0300

    Symfony 4.3 Event Dispatcher compatibility fix. https://symfony.com/blog/new-in-symfony-4-3-simpler-event-dispatching

--- src/Dispatcher/ClassEventDispatcher.php
+++ src/Dispatcher/ClassEventDispatcher.php
@@ -10,12 +10,16 @@ class ClassEventDispatcher
 {
     protected $dispatchers = array();
     protected $dispatcherClass = 'Symfony\Component\EventDispatcher\EventDispatcher';
-    
+    protected $legacyDispatcherProxyClass = 'Symfony\Component\EventDispatcher\LegacyEventDispatcherProxy';
+
+    /**
+     * @param string $dispatcherClass
+     */
     public function setDispatcherClass($dispatcherClass)
     {
         $this->dispatcherClass = $dispatcherClass;
     }
-    
+
     /**
      * @param string $class
      * @return \Symfony\Component\EventDispatcher\EventDispatcher
@@ -24,17 +28,23 @@ class ClassEventDispatcher
     {
         if (!array_key_exists($class, $this->dispatchers)) {
             $cls = $this->dispatcherClass;
+
             $this->dispatchers[$class] = new $cls();
+
+            if(class_exists($this->legacyDispatcherProxyClass)) {
+                $proxy = $this->legacyDispatcherProxyClass;
+                $this->dispatchers[$class] = $proxy::decorate($this->dispatchers[$class]);
+            }
         }
-        
+
         return $this->dispatchers[$class];
     }
-    
+
     public function addListener($class, $eventName, $listener, $priority = 0)
     {
         $this->get($class)->addListener($eventName, $listener, $priority);
     }
-    
+
     public function addSubscriber($class, EventSubscriberInterface $subscriber)
     {
         $this->get($class)->addSubscriber($subscriber);
